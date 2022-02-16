70cd3625c91519a8db68ca181fe1d0abedcc24a3
Daniel Milan <danielmilanvera@gmail.com>
2022-02-16 12:11:37 +0100

    Axios lesson

--- services/charactersApi.js
+++ services/charactersApi.js
@@ -1,6 +1,28 @@
 const axios = require("axios");
 
+// axios is the nice fetch()
+
 class CharactersApi {
+    // ADD SECRETS IN THE CONSTRUCTOR LIKE ((API_KEY, API_USERNAME))
+    constructor(baseURL){
+        this.baseURL = baseURL || "https://ih-crud-api.herokuapp.com"
+        this.api = axios.create({baseURL: this.baseURL})
+        //this.API_KEY = API_KEY
+        //this.API_USER = API_USER
+
+        // connection to api with secrets
+
+        //  this.api = axios.create({ baseURL: `${this.baseURL}/?key=${API_KEY}` });
+    }
+
+    getAllCharacters = ()=> axios.get("/characters")
+    getOneCharacter = (id)=> {
+       return this.api.get(`/characters/${id}`)
+    }
+    createOneCharacter = ()=> {}
+    updateOneCharacter = ()=> {}
+    deleteOneCharacter = ()=> {}
+
 }
 
 module.exports = CharactersApi;
